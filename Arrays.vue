8078479a9edcb2243a6fa77165958896d1bfc31d
mamatha-500apps <mamatha.gugulothu@mantragroup.com>
2022-04-01 19:08:46 +0530

    Arrays

---
+++ Arrays.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>ArraYs</h1>
+        <button @click="arr()">Click Me</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"ArrayName",
+    data(){
+       
+
+    },
+    methods:
+    {
+        arr(){
+        var a=["Hi","Hello","Welcome"]
+        var txt=""
+        for (var i=0;i<a.length;i++)
+        {
+            txt+=a[i]+"</br>";
+        }
+        document.write(txt);
+    }
+    }
+
+}
+</script>
