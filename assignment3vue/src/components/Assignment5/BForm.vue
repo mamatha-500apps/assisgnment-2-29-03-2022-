ae072b509517798d52a2b3f3eac93b42da4a9af7
mamatha-500apps <mamatha.gugulothu@mantragroup.com>
2022-04-12 19:36:53 +0530

    assignment 5 commit

---
+++ assignment3vue/src/components/Assignment5/BForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-form id="form">
+      Enter First Name:<b-form-input id="fname" class="firstname" placeholder="enter First name"></b-form-input>
+      Enter LastName:<b-form-input id="lname" class="lastname" placeholder="enter Last name">
+      </b-form-input>
+      <b-button @click="Add()">Add</b-button><br><br>
+      <b-button @click="Delete()">Delete</b-button>
+    </b-form>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [{first_name:"Agile",last_name:"CRM" }],
+      fields: ["first_name","last_name"],
+    };
+  },
+  methods: {
+    Add() {
+      this.items.push(
+     {first_name:(document.getElementById("fname").value),last_name:(document.getElementById("lname").value )}
+      );
+    },
+    Delete() {
+      this.items.pop();
+    },
+  },
+};
+</script>
---
+++ assignment3vue/src/components/Assignment5/FifthQsn5.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+<h1>FifthQsn</h1>
+<b-table striped hover :items="posts" :fields="fields"> </b-table>
+{{ posts }}
+</div>
+</template>
+<script>
+export default {
+name: "queS4",
+data() {
+return {
+posts: " ",
+};
+},
+methods: {
+async getData() {
+try {
+let response = await fetch(
+"http://universities.hipolabs.com/search?country"
+);
+this.posts = await response.json();
+} catch (error) {
+console.log(error);
+}
+},
+},
+created() {
+this.getData();
+},
+};
+
+</script>
+
+
+
+
