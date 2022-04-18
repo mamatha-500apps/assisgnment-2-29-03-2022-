ced34428a5362259297ffa62dc13564241db57ad
mamatha-500apps <mamatha.gugulothu@mantragroup.com>
2022-04-18 19:30:14 +0530

    18April

---
+++ assignment3vue/src/components/VueComponent/AxioS.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="fun()">clickme</button>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "AxiOs",
+  data() {
+    return {};
+  },
+  methods: {
+    fun() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos/1")
+        .then((response) => console.log(response.data))
+        .catch(function (error) {
+          if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
+    },
+  },
+};
+</script>
---
+++ assignment3vue/src/components/VueComponent/Fetch.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Using Fetch</h1>
+        <button @click=fun()>click me</button>
+        </div>
+</template>
+<script>
+export default {
+    name:"FeTch",   
+        data(){
+            return{
+            }
+        },
+    methods:{
+fun()
+{  
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+}
+}
+</script>
