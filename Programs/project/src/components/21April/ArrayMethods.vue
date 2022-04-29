c1584113e79bf28a25119c0ace184b5a0d7d888f
mamatha-500apps <mamatha.gugulothu@mantragroup.com>
2022-04-29 11:51:38 +0530

    21April

---
+++ Programs/project/src/components/21April/ArrayMethods.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <button @click="fun()">ForEach</button>
+        <button @click="fun1()">Find</button>
+        <button @click="fun2()">Filter</button>
+        <button @click="fun3()">Map</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+          array1 :[0,1,2,2.1,3.3,3],
+        }
+    },
+    methods:{
+        fun()
+        {
+this.array1.forEach(element =>document.write(element+"<br>"));
+        },
+        fun1()
+        {            
+const a = this.array1.find(element => element ==2);
+document.write(a);
+document.write(this.array1.findIndex(a))
+        },
+        fun2()
+        {
+   const a=this.array1.filter(element =>element==3);
+   document.write(a);
+        },
+        fun3(){
+const root = this.array1.map((num) => Math.sqrt(num));
+document.write(root);
+        },
+       
+        }
+}
+</script>
---
+++ Programs/project/src/components/21April/ComputeD.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+         FirstName : <input type = "text" v-model = "firstname" /> <br/><br/>
+         LastName : <input type = "text" v-model = "lastname"/> <br/><br/>
+        FullName: {{getfullname}}
+      </div>
+</template>
+<script>
+export default {
+  name: "QuestioN5",
+  data() {
+      return{
+    firstname :"",
+      lastname :"",
+      }
+  },
+  computed: {
+     getfullname : function(){
+         return this.firstname +" "+ this.lastname;
+      }
+  },
+};
+</script>
