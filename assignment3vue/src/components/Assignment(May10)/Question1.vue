6a8927f00ed1ef2c3149ac3592a495cd30b2e470
mamatha-500apps <mamatha.gugulothu@mantragroup.com>
2022-05-10 17:12:44 +0530

    Assignment

---
+++ assignment3vue/src/components/Assignment(May10)/Question1.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <b-form id="form">
+      Enter product name:<b-form-input id="name" class="name" placeholder="Enter product name"></b-form-input>
+      Enter product price:<b-form-input id="price" class="prices" placeholder="Enter product price:"></b-form-input>
+      Enter product category:<b-form-input id="category" class="categories" placeholder="Enter product category"></b-form-input>
+      <b-button @click="Add()">Add</b-button><br><br>
+    </b-form>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+    </div>
+</template>
+<script>
+export default ({
+    name:"QuestioN1",
+   data() {
+    return {
+      items: [{productname:"Chocolate",productprice:"10",productcategory:"chocolate"}],
+      fields: [],
+    };
+  },
+  methods: {
+    Add() {
+      this.items.push(
+     {productname:(document.getElementById("name").value),productprice:(document.getElementById("price").value),productcategory:(document.getElementById("category").value)}
+      );
+    },
+}
+})
+</script>
